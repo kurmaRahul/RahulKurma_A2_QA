4900e96a2d85f0bddf96583eaa408be5d76edb0b
kurmaRahul <122330431+kurmaRahul@users.noreply.github.com>
2023-06-08 10:34:19 -0400

    zero length and invalid response triangle test cases completed

--- NewTest/Class1.cs
+++ NewTest/Class1.cs
@@ -151,5 +151,104 @@ namespace NewTest
             Assert.AreEqual("The triangle is valid and is a SCALENE", triangleType);
         }
 
+
+
+
+        //Tests for verifying a zero length for one or more sides
+
+        [Test]
+        public void AnalyzeTriangle_Input0and2and3_OutputZeroLengthSides()
+        {
+            // Arrange
+            int sideA = 0;
+            int sideB = 2;
+            int sideC = 3;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", triangleType);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input6and0and4_OutputZeroLengthSides()
+        {
+            // Arrange
+            int sideA = 6;
+            int sideB = 0;
+            int sideC = 4;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", triangleType);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input0and9and0_OutputZeroLengthSides()
+        {
+            // Arrange
+            int sideA = 0;
+            int sideB = 9;
+            int sideC = 0;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("At least one side of your triangle has a zero length and is thus invalid", triangleType);
+        }
+
+
+
+        //Tests for verifying an invalid response (other than a zero length)
+
+        [Test]
+        public void AnalyzeTriangle_Input2and5and8_OutputInvalidResponse()
+        {
+            // Arrange
+            int sideA = 2;
+            int sideB = 5;
+            int sideC = 8;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("A triangle cannot be formed with those numbers", triangleType);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input1and2and5_OutputInvalidResponse()
+        {
+            // Arrange
+            int sideA = 1;
+            int sideB = 2;
+            int sideC = 5;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("A triangle cannot be formed with those numbers", triangleType);
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Input4and9and15_OutputInvalidResponse()
+        {
+            // Arrange
+            int sideA = 4;
+            int sideB = 9;
+            int sideC = 15;
+
+            // Act
+            string triangleType = Triangle.AnalyzeTriangle(sideA, sideB, sideC);
+
+            // Assert
+            Assert.AreEqual("A triangle cannot be formed with those numbers", triangleType);
+        }
+
     }
 }
